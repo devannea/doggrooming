da6cc6eb93e0f279f7ad69a6feffea16da6a4b09
Johnny Reina <johnny.r.reina@gmail.com>
2024-06-07 01:04:09 -0500

    feat: add react version of stuff

---
+++ react/src/Components/Contact.js
@@ -0,0 +1,17 @@
+export const Contact = () => ( <main>
+    <div class="container col-xxl-8 px-4 py-5">
+        <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+          <div class="col-sm-10 col-lg-6">
+            <img src="./assets/images/Bubs.jpg" class="d-block mx-lg-auto img-fluid rounded-4" alt="Bootstrap Themes" width="700" height="560" loading="lazy" />
+          </div>
+          <div class="col-lg-6">
+            <h1 class="display-5 fw-bold lh-1 mb-3">Let's Get In Touch</h1>
+            <p class="lead">If you would like to book an appointment with me or have any questions or concerns, feel free to reach out via text or on Facebook. You can send vaccine records to <a class="mail-to" href="mailto:doggrooming@devanneaceves.com">doggrooming@devanneaceves.com</a>.</p>
+            <div class="d-grid gap-2 d-md-flex justify-content-md-start">
+                <a href="sms:5127664929;?&body=message" class="btn btn-lg text-white me-md-2 text-btn">Text (512)766-4929</a>
+                <a href="https://www.facebook.com/devanneacevesdoggrooming" class="btn btn-outline btn-lg px-4 facebook-btn">Go To Facebook</a>
+            </div>
+          </div>
+        </div>
+    </div>       
+  </main>)
